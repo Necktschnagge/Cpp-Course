c6946c4525f9fb3e6c76b85a9cd64380161c92cf
Maximilian Starke <maximilian.starke@medionmail.com>
2017-04-20 09:15:02 +0200

    lesson

--- CPP Kurs/listings/02-basic-data-types.cpp
+++ CPP Kurs/listings/02-basic-data-types.cpp
@@ -2,8 +2,8 @@
 #include <cstdint>
 
 int main(){
-	static_assert(sizeof(int)==4,"fehler");
-	uint16_t a = 5;
-	std::cout << "Hello World und eine Zahl:\n" << a << std::endl;
+	static_assert(sizeof(int)==2,"fehler");
+	std::uint16_t a = 5;
+	std::cout << "Ausgabe der Zahl a:\n" << a << std::endl;
 	return 0;
 }
